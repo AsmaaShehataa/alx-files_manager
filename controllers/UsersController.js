4e2a97073ad7b57ba9bbdd538d91f0c3543f8231
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-06-15 03:07:29 +0300

    feat: Add UsersController with postNew endpoint
    This commit adds the UsersController class with a new endpoint: `/users`. The `postNew` method handles the creation of a new user by validating the email and password fields, checking if the user already exists in the database, hashing the password, and inserting the user into the MongoDB collection. The response includes the newly created user's ID and email.
    
    These changes enhance the functionality of the application by allowing users to be created and stored in the database.

---
+++ controllers/UsersController.js
@@ -0,0 +1,32 @@
+import dbClient from '../utils/db';
+
+const sha1 = require('sha1');
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) {
+      return res.status(400).send({ error: 'Missing password' });
+    }
+
+    const userExist = await dbClient.users.find({ email });
+    if (userExist) {
+      return res.status(400).send({ error: 'Already exist' });
+    }
+    const hashedPassword = sha1(password);
+    const nuser = {
+      email,
+      password: hashedPassword,
+    };
+    try {
+      const result = await dbClient.db.collection('users').insertOne(nuser);
+      const { _id } = result.ops[0];
+      return res.status(201).json({ id: _id, email });
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+export default UsersController;
