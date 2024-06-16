d3ec9e17d88bf72218ae744b9df9d3b681f04eb6
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-06-16 04:27:57 +0300

    refactor: Update UsersController email assignment syntax

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,11 @@
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+import sha1 from 'sha1';
+import { v4 as uuidv4 } from 'uuid';
+import { MongoClient } from 'mongodb';
 
-const sha1 = require('sha1');
+const {ObjectId} = require('mongodb');
+
 
 class UsersController {
   static async postNew(req, res) {
@@ -19,10 +24,20 @@ class UsersController {
       email: email,
       password: hashedPassword,
     };
+    static async getMe (request, response) {
+      const token = request.header('X-Token') || null;
+      if (!token) return response.status(401).send({ error: 'Unauthorized' });
+  
+      const redisToken = await RedisClient.get(`auth_${token}`);
+      if (!redisToken) return response.status(401).send({ error: 'Unauthorized' });
+  
+      const user = await DBClient.db.collection('users').findOne({ _id: ObjectId(redisToken) });
+      if (!user) return response.status(401).send({ error: 'Unauthorized' });
+
     try {
       const result = await dbClient.db.collection('users').insertOne(nuser);
       const { _id } = result.ops[0];
-      return res.status(201).json({ id: _id, email: email });
+      return res.status(200).json({ id: _id, email: email });
     } catch (error) {
       return res.status(500).json({ error: 'Internal Server Error' });
     }
