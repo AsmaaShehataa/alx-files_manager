6a30868bed2e6669a7e96e9d9e39288a70e4a9cb
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-06-16 05:16:00 +0300

    refactor: Update UsersController email assignment syntax

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,47 +1,39 @@
-import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
 import sha1 from 'sha1';
-import { v4 as uuidv4 } from 'uuid';
-import { MongoClient } from 'mongodb';
+import DBClient from '../utils/db';
+import RedisClient from '../utils/redis';
 
-const {ObjectId} = require('mongodb');
-
+const { ObjectId } = require('mongodb');
 
 class UsersController {
-  static async postNew(req, res) {
-    const { email, password } = req.body;
-    if (!email) return res.status(400).send({ error: 'Missing email' });
-    if (!password) {
-      return res.status(400).send({ error: 'Missing password' });
-    }
+  static async postNew(request, response) {
+    const userEmail = request.body.email;
+    if (!userEmail) return response.status(400).send({ error: 'Missing email' });
 
-    const userExist = await dbClient.db.collection('users').findOne({ email });
-    if (userExist) {
-      return res.status(400).send({ error: 'Already exist' });
-    }
-    const hashedPassword = sha1(password);
-    const nuser = {
-      email: email,
-      password: hashedPassword,
-    };
-    static async getMe (request, response) {
-      const token = request.header('X-Token') || null;
-      if (!token) return response.status(401).send({ error: 'Unauthorized' });
-  
-      const redisToken = await RedisClient.get(`auth_${token}`);
-      if (!redisToken) return response.status(401).send({ error: 'Unauthorized' });
-  
-      const user = await DBClient.db.collection('users').findOne({ _id: ObjectId(redisToken) });
-      if (!user) return response.status(401).send({ error: 'Unauthorized' });
+    const userPassword = request.body.password;
+    if (!userPassword) return response.status(400).send({ error: 'Missing password' });
 
-    try {
-      const result = await dbClient.db.collection('users').insertOne(nuser);
-      const { _id } = result.ops[0];
-      return res.status(200).json({ id: _id, email: email });
-    } catch (error) {
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
+    const oldUserEmail = await DBClient.db.collection('users').findOne({ email: userEmail });
+    if (oldUserEmail) return response.status(400).send({ error: 'Already exist' });
+
+    const shaUserPassword = sha1(userPassword);
+    const result = await DBClient.db.collection('users').insertOne({ email: userEmail, password: shaUserPassword });
+
+    return response.status(201).send({ id: result.insertedId, email: userEmail });
+  }
+
+  static async getMe(request, response) {
+    const token = request.header('X-Token') || null;
+    if (!token) return response.status(401).send({ error: 'Unauthorized' });
+
+    const redisToken = await RedisClient.get(`auth_${token}`);
+    if (!redisToken) return response.status(401).send({ error: 'Unauthorized' });
+
+    const user = await DBClient.db.collection('users').findOne({ _id: ObjectId(redisToken) });
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
+    delete user.password;
+
+    return response.status(200).send({ id: user._id, email: user.email });
   }
 }
 
-export default UsersController;
+module.exports = UsersController;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-import express from 'express';
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
-const router = express.Router();
+const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
@@ -18,4 +18,5 @@ router.get('/files', FilesController.getIndex);
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 router.get('/files/:id/data', FilesController.getFile);
-export default router;
+
+module.exports = router;
